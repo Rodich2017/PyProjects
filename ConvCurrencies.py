75d6a7f2c4dc101c4f24807f5366af34965d7f85
Rodich2017 <rodich@abv.bg>
2017-10-15 10:39:05 +0300

    conertion - celsius to fahrenheit - radians to degrees - currencies

---
+++ ConvCurrencies.py
@@ -0,0 +1,33 @@
+# ---------- convert BGN to USD -------------------
+# rateUSD = 1.79549
+# convValue = float(input())
+# newValue =   convValue * rateUSD
+# print(round(newValue,2))
+# -------------------------------------------------
+# ---------- convert BGN to EUR, USD or GBP -------
+# import math
+#
+# rateBGN = 1
+# rateUSD = 1.79549
+# rateEUR = 1.95583
+# rateGBP = 2.53405
+#
+# convValue = float(input())
+# convCurrIn = input()
+# convCurrOUT = input()
+# newValue = 0.00
+#
+# if convCurrOut == 'BGN':
+#     if convCurrIn == 'USD':
+#         newValue = convValue * rateUSD
+#     if convCurrIn == 'EUR':
+#         newValue = convValue * rateEUR
+#     if convCurrIn == 'GBP':
+#         newValue = convValue * rateGBP
+#     if convCurrIn == 'BGN':
+#         newValue = convValue
+#
+# print(round(newValue,2))
+#
+# -------------------------------------------------
+
